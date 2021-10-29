00485566caa92876793f47e4a60223e7503e0a62
Tanmoy Tushar <46753634+tanmoy069@users.noreply.github.com>
2021-10-29 12:57:27 +0600

    Initilaized EmpDeptService microservice

---
+++ src/main/java/com/tanmoy/empdeptservice/EmpDeptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tanmoy.empdeptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDeptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDeptServiceApplication.class, args);
+	}
+
+}
