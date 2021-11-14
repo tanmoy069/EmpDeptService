bc763f835ccebc4b9e16ee27c6bb2146893d5380
tanmoy <pdt.tanmoy@gmail.com>
2021-11-14 10:09:01 +0600

    Added @@CrossOrigin to skip cors error

--- src/main/java/com/tanmoy/empdeptservice/controller/EmpDeptController.java
+++ src/main/java/com/tanmoy/empdeptservice/controller/EmpDeptController.java
@@ -3,6 +3,7 @@ package com.tanmoy.empdeptservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.tanmoy.empdeptservice.model.DepartmentList;
 import com.tanmoy.empdeptservice.model.Employee;
 import com.tanmoy.empdeptservice.model.EmployeeList;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/empdept")
 public class EmpDeptController {
